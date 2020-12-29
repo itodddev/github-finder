902d93f3592415646d821c9c28958cd17f5e1e07
Todd James <todd@itodd.co>
2020-12-29 07:22:19 -0500

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from React</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <Navbar title="Github Finder" icon="fab fa-github" /> */}
+        {/* Using defaultProps on Navbar component, so you dont have to pass props
+            if you do send props they will overwrite the defaultProps */}
+        <Navbar />
+      </div>
+    );
+  }
 }
 
 export default App;
